126406afe826709f1c923d5a33b2986ac1caebeb
Alexander-en <knowledge2450@gmail.com>
2026-08-13 20:40:41 +0500

    Fix dynamic product pages

--- src/app/product/listing/women/page.js
+++ src/app/product/listing/women/page.js
@@ -1,10 +1,8 @@
 import CommonListing from "@/components/CommonListing";
-import connectToDB from "@/database";
-import Product from "@/models/product";
+import { productByCategory } from "@/services/product";
 
 export default async function WomenAllProducts() {
-  await connectToDB();
-  const getAllProducts = await Product.find({ category: "woman" }).lean();
+  const getAllProducts = await productByCategory("woman");
 
-  return <CommonListing data={getAllProducts} />;
+  return <CommonListing data={getAllProducts && getAllProducts.data} />;
 }
